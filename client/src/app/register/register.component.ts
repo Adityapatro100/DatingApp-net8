0989e497035dffbf97146afff7cc161c2f627773
Adityapatro100 <adityapatro100@gmail.com>
2024-10-13 17:04:06 +0530

    End_of_section_6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component,inject, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
 
   // @Output() CancelRegister = new EventEmitter();
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   CancelRegister = output<any>();
   model:any = {};
 
@@ -22,7 +24,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.error()      
+      error: error => this.toastr.error(error.error)
     })
   }
 
